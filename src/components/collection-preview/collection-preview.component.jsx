dfb53fbe04efd2bf8e4bc08ea90aedcda33bf28e
Deyvison Ramos <deyvisonr0@gmail.com>
2020-04-05 20:15:50 -0300

    feat: adds a shop page, collection preview component and collection item component

---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./collection-preview.styles.scss";
+import CollectionItem from "../collection-item/collection-item.component";
+
+const CollectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <div className="preview">
+      {items
+        .filter((_, index) => index < 4)
+        .map(({ id, ...item }) => (
+          <CollectionItem key={id} {...item} />
+        ))}
+    </div>
+  </div>
+);
+
+export default CollectionPreview;
